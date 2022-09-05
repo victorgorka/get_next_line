affb46be04604d53f071b312b90ef97f88f11b79
Victor gorka De prada arnedo <vde-prad@student.42malaga.com>
2022-09-05 17:32:50 +0200

    con bugs

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: vde-prad <vde-prad@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 15:01:33 by vde-prad          #+#    #+#             */
-/*   Updated: 2022/09/01 19:27:37 by vde-prad         ###   ########.fr       */
+/*   Updated: 2022/09/05 17:27:02 by vde-prad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
-
+//Recibe el buffer y copia todos los caracteres despues del primer salto de
+//linea y lo devuelve con reserva de memoria
 char	*ft_save_chars(const char *buff)
 {
 	unsigned int	before;
@@ -30,25 +31,29 @@ char	*ft_save_chars(const char *buff)
 	saved = ft_strdup(buff);
 	return (saved);
 }
+/*
+   char	*ft_get_line(char *buff)
+   {
+   char	*line;
 
-char	*ft_get_line(char *buff)
-{
-	char	*line;
-
-	line = ft_strdup(buff);
-	return (line);
-}
-
+   line = ft_strdup(buff);
+   return (line);
+   }
+   */
+//Funcion que lee el fichero tantas veces que sea necesario hasta encontrar un
+//salto de linea con reserva de memoria
 void	ft_read_fd(int fd, char **buff)
 {
 	char	got[BUFFER_SIZE];
 	char 	*temp;
 	int 	len;
-	
+
 	temp = *buff;
 	len = 0;
-	while (!ft_strchr(*buff, '\n'))
+	puts("read");
+	while (ft_strchr(*buff, '\n') != *buff)
 	{
+		puts("read");
 		len = read(fd, got, BUFFER_SIZE);
 		if (len == -1)
 		{
@@ -65,41 +70,53 @@ char 	*get_next_line(int fd)
 	char		*line;
 	static char	*buff;
 
-	*buff = "";
 	if (fd <= 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	puts("get_next");
 	ft_read_fd(fd, &buff);
+	puts("get_next");
 	if(!buff)
 		return (NULL);
-	line = ft_get_line(buff);
+	puts("get_next");
+	line = ft_strdup(buff);
 	buff = ft_save_chars(buff);
-	if (*line == 0 || *line == "")
+	if (*line == 0)
 		return (NULL);
 	return (line);
 }
 /*
 int main()
 {
-	char *line = "Hola\n mundo";
+char *line = "Hola\n mundo";
 
-	line = ft_save_chars(line);
-	puts(line);
-	free(line);
-	return (0);
+line = ft_save_chars(line);
+puts(line);
+free(line);
+return (0);
 }
 */
 
 int	main(void)
 {
-	int fd;
+	int 	fd;
+	char	*line;
+	int		i;
 
-//	We open the file and assign the returned file descriptor
-//	to the fd variable in order to use it later:
-	fd = open("text.txt", O_READONLY);
-//	Was correctly open?
+	i = 0;
+	//	We open the file and assign the returned file descriptor
+	//	to the fd variable in order to use it later:
+	fd = open("text.txt", O_RDONLY);
+	//	Was correctly open?
 	if(fd == -1)
 		return (-1);
-//	Close the file
+	while (i != 2)
+	{
+		i++;
+		line = get_next_line(fd);
+		puts(line);
+	}
+	free(line);
+	//	Close the file
 	close(fd);
 
 	return (0);
